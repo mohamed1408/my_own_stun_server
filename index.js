c85e73c6b5cb44ed1acd1fcc2e353560b8ea6361
Karthick <karthick.nath@gmail.com>
2021-01-11 20:02:11 +0530

    cbcvb

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+const express = require('express');
+const api = express();
+var cors = require('cors')
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+api.use(cors());
+
+api.get('/getmyip', (request, response) => {
+    var ip = request.ip;
+    var data = {ip: ip}
+    response.send(data);
+});
+
+var server = api.listen(process.env.PORT, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
